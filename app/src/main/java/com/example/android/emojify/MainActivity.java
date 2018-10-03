cc6b50e4f6041dff0f07be9546e6172bc64163e6
Evan <evan.pend@gmail.com>
2018-10-02 21:48:59 -0700

    Exercise 6 progress

--- app/src/main/java/com/example/android/emojify/MainActivity.java
+++ app/src/main/java/com/example/android/emojify/MainActivity.java
@@ -39,6 +39,10 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     // TODO (2): Replace all View declarations with Butterknife annotations
@@ -50,9 +54,10 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView mImageView;
 
-    private Button mEmojifyButton;
-    private FloatingActionButton mShareFab;
-    private FloatingActionButton mSaveFab;
+
+    @BindView(R.id.emojify_button) Button mEmojifyButton;
+    @BindView(R.id.share_button) FloatingActionButton mShareFab;
+    @BindView(R.id.save_button) FloatingActionButton mSaveFab;
     private FloatingActionButton mClearFab;
 
     private TextView mTitleTextView;
@@ -70,11 +75,10 @@ public class MainActivity extends AppCompatActivity {
         // TODO (3): Replace the findViewById calls with the Butterknife data binding
         // Bind the views
         mImageView = (ImageView) findViewById(R.id.image_view);
-        mEmojifyButton = (Button) findViewById(R.id.emojify_button);
-        mShareFab = (FloatingActionButton) findViewById(R.id.share_button);
-        mSaveFab = (FloatingActionButton) findViewById(R.id.save_button);
         mClearFab = (FloatingActionButton) findViewById(R.id.clear_button);
         mTitleTextView = (TextView) findViewById(R.id.title_text_view);
+
+        ButterKnife.bind(this);
     }
 
     /**
@@ -199,7 +203,8 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view The save button.
      */
-    public void saveMe(View view) {
+    @OnClick(R.id.save_button)
+    public void saveMe() {
         // Delete the temporary image file
         BitmapUtils.deleteImageFile(this, mTempPhotoPath);
 
